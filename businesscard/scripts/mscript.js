4b8615309a88298389a6fbce57c1c9f9f49cab6b
mlionello <lionello.matteo@gmail.com>
2022-05-29 16:47:09 +0200

    swipe

--- businesscard/scripts/mscript.js
+++ businesscard/scripts/mscript.js
@@ -196,6 +196,9 @@ function handleTouchMove(evt) {
     if ( ! xDown || ! yDown ) {
         return;
     }
+    if(window.innerHeight > window.innerWidth){
+        return;
+    }
 
     var xUp = evt.touches[0].clientX;
     var yUp = evt.touches[0].clientY;
@@ -208,12 +211,6 @@ function handleTouchMove(evt) {
             turnpage(idListinit[idListinit.lastIndexOf(current_id)+1])
         } else {
             turnpage(idListinit[Math.max(idListinit.lastIndexOf(current_id)-1,0)])
-        }
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */
-        } else {
-            /* down swipe */
         }
     }
     /* reset values */
