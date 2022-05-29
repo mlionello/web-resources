2971cbc2fb8dc7584e3c26b52074a610c87ea30b
mlionello <lionello.matteo@gmail.com>
2022-05-29 16:30:36 +0200

    border radius, padding

--- businesscard/scripts/mscript.js
+++ businesscard/scripts/mscript.js
@@ -173,3 +173,23 @@ function enableScroll() {
         }}}
   document.getElementById("menupointer").innerHTML = idList[current_order]
 });
+
+
+let touchstartX = 0
+let touchendX = 0
+
+const slider = document.getElementById('slider')
+
+function handleGesture() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+slider.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+slider.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
